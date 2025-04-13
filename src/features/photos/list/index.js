a36d8d624e840d0ebcd94b94be6c2e6c99a30809
Sahand Raoufi <sahand.raoufi@email.com>
2025-04-13 15:15:29 -0600

    complete all tasks

--- src/features/photos/list/index.js
+++ src/features/photos/list/index.js
@@ -1,28 +1,28 @@
 import { useSelector, useDispatch } from 'react-redux';
 import {
-  // Task 7: Import the `removePhoto()` action creator from the photos slice
-  selectAllPhotos,
+  removePhoto,
+  selectFilteredPhotos,
   // Task 13: Import the `selectFilteredPhotos()` selector from the photos slice
 } from '../photos.slice';
 import './list.css';
 
 export default function PhotosList() {
   // Task 14: Call `useSelector()` below with `selectFilteredPhotos` instead of `selectAllPhotos`
-  const photos = useSelector(selectAllPhotos);
-  // Task 8: Store a reference to the Redux store's dispatch method in a variable called `dispatch`
+  const photos = useSelector(selectFilteredPhotos);
+  const dispatch = useDispatch();
 
   function handleDeleteButtonClick(id) {
-    // Task 9: Dispatch the `removePhoto()` action creator, passing in the id
+    dispatch(removePhoto(id));
   }
 
-  const photosListItems = photos.map(({ id, caption, imageUrl }) => (
-    <li key={id}>
-      <img alt={caption} src={imageUrl} />
+  const photosListItems = photos.map((photo) => (
+    <li key={`photo-${photo.id}`}>
+      <img alt={photo.caption} src={photo.imageUrl} />
       <div>
-        <p>{caption}</p>
+        <p>{photo.caption}</p>
         <button
-          data-testid={`${caption}-button`}
-          onClick={() => handleDeleteButtonClick(id)}>
+          data-testid={`${photo.caption}-button`}
+          onClick={() => handleDeleteButtonClick(photo.id)}>
           Delete
         </button>
       </div>
